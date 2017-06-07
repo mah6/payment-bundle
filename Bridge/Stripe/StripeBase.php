477e7fc47eef8a3773657cbbb6d132c639a66c33
stephn <m6ahenina@gmail.com>
2017-06-07 13:27:35 +0300

    stripe Bridge

---
+++ Bridge/Stripe/StripeBase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PaymentBundle\Bridge\Stripe;
+
+use Stripe\Stripe;
+use Stripe\Charge;
+use Stripe\Customer;
+use Stripe\Plan;
+use Stripe\Subscription;
+
+class StripeBase
+{
+	protected $apiKeys;
+	protected $em;
+	protected $tokenStorage;
+
+	public function __construct($apiKeys, EntityManager $em, TokenStorage $tokenStorage)
+	{
+		$this->apiKeys = $apiKeys;
+		$this->em = $em;
+		$this->tokenStorage = $tokenStorage;
+		Stripe::setApiKey($this->apiKeys['secret_key']);
+	}
+
+	public function bind($stripeObject, $entity)
+	{
+		$vars = get_object_vars($stripeObject);
+
+		foreach ($vars as $property => $value) {
+
+			$this->callUserFunc($entity, $property, $value, 'set');
+
+		}
+	}
+
+	public static function callUserFunc($object, $property, $params, $prefix = 'get')
+	{
+		$method = $prefix . ucfirst($property);
+
+		if (method_exists($object, $method)) {
+			return call_user_func([$object => $method], $params);
+		}
+
+		return ;
+	}
+
+}
---
+++ Bridge/Stripe/StripeSubscription.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PaymentBundle\Bridge\Stripe;
+
+use Symfony\Component\OptionsResolver;
+
+use Stripe\Stripe;
+use Stripe\Charge;
+use Stripe\Customer;
+use Stripe\Plan;
+use Stripe\Subscription;
+
+class StripeSubscription
+{
+	public function __construct($apiKeys, EntityManager $em, TokenStorage $tokenStorage)
+	{
+		parent::__construct($apiKeys, $em, $tokenStorage);		
+	}
+
+	public function createSubscription()
+	{
+		$subscription = Subscription::create([
+			'customer' => $customerId,
+			'plan' => $planId,
+		]);
+
+		return $subscription;
+	}
+
+	public function configureOptions(array $options)
+	{
+		$resolver = new OptionsResolver();
+
+		return $resolver->resolve($options);
+	}
+}
